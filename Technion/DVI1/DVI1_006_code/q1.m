2e49fd1bc5432b27cfd8ece3eb49ec1bf39545ff
nucleargandhi <dodobentov@gmail.com>
2025-01-07 22:02:28 +0200

    Add content prep

--- Technion/DVI1/DVI1_006_code/q1.m
+++ Technion/DVI1/DVI1_006_code/q1.m
@@ -1,15 +1,17 @@
 clc; clear; close all;
 
+%% Without damping
+
 % Define system parameters
 m3 = 1;
-l1 = 1; % Length of the first rod
-l2 = 1; % Length of the second rod
 l3 = 1;
+l1 = (10/7)^(1/2);
+l2 = (15/7)^(1/2);
 omega_0 = 1;
 natural_frequencies = [omega_0, 2*omega_0, 3*omega_0, 4*omega_0, 5*omega_0]; % Natural frequencies
 
-m1 = 10/7*m3*(l3/l1)^2;
-m2 = 15/14*m3*(l3/l2)^2;
+m1 = (7/10)*m3*(l3/l1)^2;
+m2 = (14/15)*m3*(l3/l2)^2;
 
 m = [m1 m2 m3 m2 m1]; % Masses
 l = [l1 l2 l3 l2 l1]; % Lengths
@@ -38,10 +40,10 @@ alpha = 1 ./ sqrt(modal_mass);
 modes = unnormalized_modes * diag(alpha);
 
 % Create the 3D animation for each mode separately
-for mode_idx = 4:5
+for mode_idx = 5:5
     % Initial conditions in modal coordinates to excite only one mode
     eta0 = zeros(10, 1);
-    eta0(mode_idx) = 0.5; % Excite only the current mode
+    eta0(mode_idx) = 1; % Excite only the current mode
 
     % Time span for 10 cycles of the current natural frequency
     tspan = linspace(0, 1 * (2 * pi / natural_frequencies(mode_idx)), 100);
@@ -54,17 +56,19 @@ for mode_idx = 4:5
 
     % Create the 3D animation for the current mode
     figure('Color', 'w'); % Set figure background to white
+    set(gcf, 'Units', 'pixels', 'Position', [300, 300, 600, 400]); % Set figure size and position
     axis equal;
     view(3); % Set the view to 3D
     hold on;
     set(gca, 'Color', [1 1 1]); % Set 3D plot background to white
     masses = gobjects(5, 2);
     rods = gobjects(5, 1);
-    colors = ['b', 'r', 'g', 'm', 'c'];
+    % matlab default colors
+    colors = get(gca, 'ColorOrder');
 
     for i = 1:5
-        masses(i, 1) = animatedline('Marker', 'o', 'MarkerSize', 8, 'MarkerFaceColor', colors(i), 'Color', colors(i));
-        masses(i, 2) = animatedline('Marker', 'o', 'MarkerSize', 8, 'MarkerFaceColor', colors(i), 'Color', colors(i));
+        masses(i, 1) = animatedline('Marker', 'o', 'MarkerSize', 5 * log(10 * m(i)), 'MarkerFaceColor', colors(i, :), 'Color', colors(i, :));
+        masses(i, 2) = animatedline('Marker', 'o', 'MarkerSize', 5 * log(10 * m(i)), 'MarkerFaceColor', colors(i, :), 'Color', colors(i, :));
         rods(i) = animatedline('LineWidth', 2, 'Color', 'k');
     end
 
@@ -93,16 +97,62 @@ for mode_idx = 4:5
         end
         drawnow; % Update the animation
 
-        % Capture the plot as an image
-        frame = getframe(gcf);
-        im = frame2im(frame);
-        [imind, cm] = rgb2ind(im, 256);
+        % % Capture the plot as an image
+        % frame = getframe(gcf);
+        % im = frame2im(frame);
+        % [imind, cm] = rgb2ind(im, 256);
 
-        % Write to the GIF File
-        if i == 1
-            imwrite(imind, cm, filename, 'gif', 'Loopcount', inf, 'DelayTime', 0.05);
-        else
-            imwrite(imind, cm, filename, 'gif', 'WriteMode', 'append', 'DelayTime', 0.05);
-        end
+        % % Write to the GIF File
+        % if i == 1
+        %     imwrite(imind, cm, filename, 'gif', 'Loopcount', inf, 'DelayTime', 0.02);
+        % else
+        %     imwrite(imind, cm, filename, 'gif', 'WriteMode', 'append', 'DelayTime', 0.02);
+        % end
     end
+end
+
+%% With damping
+
+modal_zeta = [0.1 0.1 0.1 0.1 0.1]; % Modal damping ratios
+
+% Define the differential equations in modal coordinates
+dynamics = @(t, eta) [eta(6:10);
+    -natural_frequencies(1)^2 * eta(1) - 2 * modal_zeta(1) * natural_frequencies(1) * eta(6);
+    -natural_frequencies(2)^2 * eta(2) - 2 * modal_zeta(2) * natural_frequencies(2) * eta(7);
+    -natural_frequencies(3)^2 * eta(3) - 2 * modal_zeta(3) * natural_frequencies(3) * eta(8);
+    -natural_frequencies(4)^2 * eta(4) - 2 * modal_zeta(4) * natural_frequencies(4) * eta(9);
+    -natural_frequencies(5)^2 * eta(5) - 2 * modal_zeta(5) * natural_frequencies(5) * eta(10)];
+
+% Initial conditions in modal coordinates
+eta0 = [10 0 10 0 10 0 0 0 0 0;
+    0 10 0 -10 0 0 0 0 0 0];
+eta0 = deg2rad(eta0);
+
+tspan = linspace(0, 15, 1000);
+
+
+plot_title = {'Free Vibration with Symmetric initial conditions', 'Free Vibration with Anti-Symmetric initial conditions'};
+line_style = {'-', ':', ':', ':', ':'};
+for i = 1:size(eta0, 1)
+    figure;
+    set(gcf, 'Units', 'pixels', 'Position', [300, 300, 800, 400]); % Set figure size and position
+    % Solve the differential equations
+    [t, eta] = ode45(dynamics, tspan, eta0(i, :));
+
+    % Convert modal coordinates to physical coordinates
+    theta = (modes * eta(:, 1:5)')';
+    theta = rad2deg(theta);
+
+    % Plot the physical coordinates
+    for j = 1:5
+        plot(t, theta(:, j), 'LineWidth', 2, 'LineStyle', line_style{j});
+        hold on;
+    end
+
+    title(plot_title{i}, 'Interpreter', 'none');
+    xlabel('$t$ (s)', 'Interpreter', 'latex');
+    ylabel('$\theta$ (degrees)', 'Interpreter', 'latex');
+    legend(arrayfun(@(x) ['$\theta_', num2str(x), '$'], 1:5, 'UniformOutput', false), 'Interpreter', 'latex');
+    grid on;
+    exportgraphics(gcf, ['q1_free_vibration_', num2str(i), '.png'], 'Resolution', 300);
 end
