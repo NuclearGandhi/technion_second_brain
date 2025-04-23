52c8b3685d50431a131722e4583a41f1b8f7d150
NuclearGandhi <dodobentov@gmail.com>
2025-04-23 23:13:40 +0300

    Add material

---
+++ Technion/LPH1/LPH1_L001/code/wmp.m
@@ -0,0 +1,58 @@
+%% Parameters
+m = 32.74e-3;
+delta_m = 0.01e-3; % kg
+
+d = 20e-3; % m
+delta_d = 0.1e-3; % m
+
+l = [0.2]; % m
+
+%% deg_var
+initial_deg = linspace(90, 10, 9); % deg
+T_deg_var = [10.31, 9.94, 9.76, 9.52, 9.28, 9.07, 9.03, 8.95, 8.9]; % s
+
+% Plot T_deg_var vs initial_deg
+figure(1)
+plot(initial_deg, T_deg_var, 'o-')
+xlabel('Initial\ angle\ (deg)', 'Interpreter', 'latex')
+ylabel('Period $T(s)$', 'Interpreter', 'latex')
+title('Period\ vs\ Initial\ Angle', 'Interpreter', 'latex')
+grid on
+axis([0 100 8 11])
+legend({'$T$'}, 'Interpreter', 'latex')
+hold on
+
+% Customize x-axis to show degree symbol
+xticks(0:10:100)
+xticklabels(arrayfun(@(x) sprintf('%d°', x), 0:10:100, 'UniformOutput', false))
+
+%% length variation
+l = linspace(0.2, 0.47, 10); % m
+delta_l = 1e-3; % m
+initial_deg = 30; % deg
+
+T_len_var = [9.13, 9.63, 10.25, 10.64, 11.41, 11.94, 12.4, 12.81, 13.54, 13.83]; % s
+
+% Plot ln(l / l(end)) vs ln(T_len_var / T_len_var(end))
+figure(2)
+x = log(T_len_var / T_len_var(end));
+y = log(l / l(end));
+% Swap x and y for the new plot
+plot(y, x, 'o-')
+title('Plot of $\frac{\ln(T/T^{*})}{\ln(\ell /{\ell}^{*})}$', 'Interpreter', 'latex')
+grid on
+legend({''}, 'Interpreter', 'latex')
+axis([-1 0.2 -0.5 0.2])
+hold on
+
+% Linear regression using Curve Fitting Toolbox (swapped axes)
+fitresult = fit(y', x', 'poly1');
+plot(fitresult, y, x)
+legend({'', '$Linear\ Fit$'}, 'Interpreter', 'latex')
+xlabel('$\ln(\ell / \ell^*)$', 'Interpreter', 'latex')
+ylabel('$\ln(T / T^*)$', 'Interpreter', 'latex')
+
+% Display the the fit equation on the graph
+slope = fitresult.p1;
+intercept = fitresult.p2;
+text(-0.5, 0.0, sprintf('linear fit: y = %.4fx + %.4f', slope, intercept), 'FontSize', 12, 'Color', 'k', 'Interpreter', 'latex')
